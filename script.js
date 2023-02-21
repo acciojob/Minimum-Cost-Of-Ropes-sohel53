ca3fe3b2174a558e215d48da3794efcef21a7959
Mahebub Maniyar <69462227+sohel53@users.noreply.github.com>
2023-02-22 02:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function calculateMinCost() {
 	arr.sort(function(a,b) {return (a-b);})
 	var cost = 0;
 	while(arr.length >1) {
-		var result = Number(arr[0] + Number(arr[1]))
+		var result = Number(arr[0]) + Number(arr[1])
 		arr.splice(0,2);
 		arr.push(result);
 		cost += result;
